6e11678954162e878e36b663600616e8979219b3
philwareham <phil@designhive.com>
2021-02-19 12:24:03 +0000

    Replace reCAPTCHA
    See https://github.com/textpattern/textpattern-forum/issues/438 for details.

---
+++ lang/English/copyable_captcha.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang_copyable_captcha = array (
+	'Captcha'        => 'Captcha',
+	'Captcha legend' => 'Confirm that you’re human',
+	'Captcha info'   => '<strong>Captcha valid for 2 minutes only.</strong> Refresh page if needed.<br>Please enter these characters: <strong class="masq">%1$s</strong>',
+	'Captcha error'  => 'Incorrect or expired captcha.',
+);
